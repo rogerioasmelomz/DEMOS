61a2ee7c3adb3bcd2de76ec13771a22c1add5547
rogerio.melo <rogerio.melo@ROGERIOMELO>
2016-07-21 22:19:17 +0200

    inicial

---
+++ Ambrosio/Ambrosio/form_Settings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ambrosio
+{
+    public partial class form_Settings : Form
+    {
+        public form_Settings()
+        {
+            InitializeComponent();
+        }
+    }
+}
